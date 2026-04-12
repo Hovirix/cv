26042173ba9149a2f2413a85317062efcefb43b4
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-04-12 03:17:34 +0200

    chore: add Nix flake with devShell

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "HX CV dev-shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go-task
+            sops
+            age
+            texliveFull
+            python3
+            python313Packages.pyyaml
+            python313Packages.pydantic
+            python313Packages.email-validator
+            python313Packages.jinja2
+            python313Packages.typer
+          ];
+        };
+      }
+    );
+}
